3dc81a3234e515f941e98da63b77f3892bc8d815
iqbal-hossain <mdiqbalhossain5170@gmail.com>
2022-03-25 23:16:23 +0600

    front page added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import Books from './components/Books/Books';
 
 function App() {
+
+  const [books, setBooks] = useState([]);
+
+
+  useEffect(() => {
+    fetch('books.json')
+      .then(res => res.json())
+      .then(data => setBooks(data));
+  }, [books])
+
   return (
     <div className="App">
-     
+      <div className="books-container">
+      {
+        books.map(book => <Books books={book} key={book.id}></Books>)
+      }
+      </div>
+    
     </div>
   );
 }
---
+++ src/components/Books/Books.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { RiShoppingCart2Fill } from 'react-icons/ri';
+import './Books.css'
+
+const Books = ( {books} ) => {
+   console.log(books);
+   const {name, picture, price} = books;
+    return (
+        <div className="books-info">
+           <img src={picture} alt="" />
+           <h1>{name}</h1>
+           <h2>${price}</h2>
+           <div className="add-to-cart">
+               <button className="add-btn">Add to Cart </button>
+               <RiShoppingCart2Fill size='30px' color='#485368'/>
+           </div>
+        </div>
+        
+    );
+};
+
+export default Books;
