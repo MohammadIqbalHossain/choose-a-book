39c5bdd947ec35723ba92ac198494e47a1ecbe80
iqbal-hossain <mdiqbalhossain5170@gmail.com>
2022-03-26 22:04:34 +0600

    some comments added

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import { AiOutlineClose } from 'react-icons/ai';
 
 function App() {
 
+  //useStates 
   const [books, setBooks] = useState([]);
   const [modalIsOpen, setIsOpen] = useState(false);
   const [cart, setCart] = useState([]);
@@ -27,27 +28,26 @@ function App() {
     },
   };
 
-  const modalStyles = {
-    overflow: 'scroll',
-    maxHeight: '100vh',
-    
 
-  }
   Modal.setAppElement('#root');
 
+  //Function for opening modal
   function openModal() {
     setIsOpen(true);
   }
 
+  //Function for closing modal
   function closeModal() {
     setIsOpen(false);
   }
 
+  //Function for Adding books in the cart.
   const addToCart = (books) => {
     const newCart = [...cart, books];
     setCart(newCart);
   }
 
+  //Function for getting a random item from added cart.
   const getRandomItem = () => {
     const randomNum = cart[Math.floor(Math.random() * cart.length)];
     let newCart = [];
@@ -56,13 +56,14 @@ function App() {
   }
 
 
+  //Funciton for Choose again button in the modal cart.
   const chooseAgain = () => {
     closeModal()
     let newCart = [];
     setCart(newCart);
   }
 
-
+  //Loading data from lcoal file.
   useEffect(() => {
     fetch('books.json')
       .then(res => res.json())
@@ -72,31 +73,38 @@ function App() {
   return (
     <div className="App">
       <Header cart={cart} openModal={openModal}></Header>
+
       <div className="books-container">
         {
           books.map(book => <Books books={book} key={book.id} addToCart={addToCart}></Books>)
         }
       </div>
+
       <Modal
         isOpen={modalIsOpen}
         onRequestClose={closeModal}
         style={customStyles}
       >
-        <div  style={modalStyles}>
-          <button className="close" onClick={closeModal}><AiOutlineClose /></button>
-          <div>
-            {
-              cart.map(book => <Cart book={book} key={book.id}></Cart>)
-            }
-          </div>
-          <div className="modal-btns">
-            <button onClick={getRandomItem} className="draw-btn">Choose One <FcReading size='25px' /> </button>
-            <br />
-            <br />
-            <button className="draw-btn" onClick={chooseAgain}>Choose Again <BiCut color='red' /></button>
-          </div>
-          </div>
+        <div>
+          
+            <button className="close" onClick={closeModal}><AiOutlineClose /></button>
+
+            <div>
+              {
+                cart.map(book => <Cart book={book} key={book.id}></Cart>)
+              }
+            </div>
+
+            <div className="modal-btns">
+              <button onClick={getRandomItem} className="draw-btn">Choose One <FcReading size='25px' /> </button>
+              <br />
+              <br />
+              <button className="draw-btn" onClick={chooseAgain}>Choose Again <BiCut color='red' /></button>
+           </div>
+
+        </div>
       </Modal>
+
       <Answers></Answers>
     </div>
   );
